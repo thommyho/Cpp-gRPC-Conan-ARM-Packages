a48fdace92e7e4a95e93141dd82b2829e0574111
Thomas Hochstrasser <thommyho1988@gmail.com>
2022-07-04 16:46:09 +0200

    Added

---
+++ build.sh
@@ -0,0 +1,1 @@
+conan create conan-scripts/conan-center-index/recipes/grpc/all grpc/1.46.3@ -pr:b profiles/x86_64_gcc9_release_host -pr:h profiles/armv7hf_gcc9_release_target -b missing -s build_type=Release
