b3a69473226b29e68b8dbd9cf695354a604cf928
Thomas <thommyho1988@gmail.com>
2022-11-19 19:41:26 +0000

    added profiles

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-conan create conan-scripts/conan-center-index/recipes/grpc/all grpc/1.46.5@ -pr:b profiles/x86_64_gcc9_release_host -pr:h profiles/armv7hf_gcc9_release_target -b missing -s build_type=Release
+conan create conan-scripts/conan-center-index/recipes/grpc/all grpc/1.46.3@ -pr:b profiles/x86_64_gcc9_release_host -pr:h profiles/armv7hf_gcc9_release_target -b missing -s build_type=Release
+conan create conan-scripts/conan-center-index/recipes/grpc/all grpc/1.46.3@ -pr:b profiles/x86_64_gcc9_debug_host -pr:h profiles/armv7hf_gcc9_debug_target -b missing -s build_type=Debug
---
+++ upload.sh
@@ -0,0 +1,1 @@
+conan upload "*" -r $ARTIFACTORY_REMOTE_NAME_ARMV7 --all --confirm
